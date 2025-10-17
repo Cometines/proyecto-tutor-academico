3a3786433949eb27ebe00266af80a2a72b271277
Leonardo Mendez <leomen27@hotmail.com>
2025-10-17 09:18:07 -0600

    Definir cabeceras para reportes.c

--- headers/captura.h
+++ headers/captura.h
@@ -1,7 +1,8 @@
 #ifndef CAPTURA_H //if not defined (si no esta definido)
 #define CAPTURA_H //se define la libreria para que no se duplique 
- extern int calificaciones[40][5];
- extern char nombresEstudiantes[40][25];
- extern int numeroEstudiantes=0;
- extern int numeroEvaluaciones=0;
+extern int calificaciones[40][5];
+extern char nombresEstudiantes[40][25];
+extern int numeroEstudiantes=0;
+extern int numeroEvaluaciones=0;
+
 #endif
---
+++ headers/reportes.h
@@ -0,0 +1,15 @@
+#ifndef REPORTES_H //if not defined (si no está definido) reportes.h
+#define REPORTES_H //entonces lo definimos, para evitar que se duplique
+
+extern int calificaciones[40][5];
+extern char nombresEstudiantes[40][25];
+extern int numeroEstudiantes=0;
+extern int numeroEvaluaciones=0;
+
+void mostrarReportes();
+void mostrarPromedioEvaluacion();
+void mostrarPromedioAlumno();
+void generarRanking();
+void mostrarPeorCalificacion();
+
+#endif
