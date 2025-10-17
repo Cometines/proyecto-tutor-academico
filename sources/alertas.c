877ac7874156a869ef2a4b74017a77d3a96872a4
FegeTECH <ervinantoniofelicianogonzalez@gmail.com>
2025-10-17 11:03:49 -0600

    Avance de la funcion alertas

--- sources/alertas.c
+++ sources/alertas.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include "../headers/alertas.h" //Subir para encontrar al arichivo
+/*
+emitirAlertas(){
+    for(int columnas=0;columnas<5;columnas++){
+        for(int filas=0;filas<40;filas++){
+            if(calificaciones[filas][columnas]<=70){
+                printf("El alumno:  %c, esta en riesgo");
+            }
+
+        }
+    }
+
+
+
+}
+*/
