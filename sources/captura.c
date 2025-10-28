1f2febffcc88405f174246a92657281ff35097d9
Liss Garcia <olgalissethgarciagomez@gmail.com>
2025-10-28 08:43:29 -0600

    Implementar parcialmente la funcion de capturas

---
+++ sources/captura.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "../headers/captura.h"
+
+void capturaDatos()
+{
+    //decir que datos ocupamos para hacer el merequetengue
+    printf("\nIntroduzca la cantidad de alumnos: \n");
+    scanf("%d",&numeroEstudiantes);
+    printf("\ningrese la cantidad de evaluaciones en este semestre:\n");
+    scanf("%d",numeroEvaluaciones);
+   // printf("\nIngrese los nombres de los alumnos\n");
+   // scanf("%c",nombresEstudiantes);
+    //printf("\nIngrese la calificacion\n");
+    //scanf("%d",calificaciones);
+for(int i=0;i<numeroEstudiantes;i++)
+{
+    printf("Ingrese el nombre del alumno: ");
+    scanf("%c",&nombresEstudiantes[i]);
+    for()
+    {
+        
+    }
+
+}
+
+
+}
