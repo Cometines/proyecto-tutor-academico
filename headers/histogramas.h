1bd8aef8b8e701f8413ce936d5852bda57d662c7
KeReT0 <kelaiareyestorres@gmail.com>
2025-11-06 23:16:40 -0600

    Cambios hechos: Documenté los archivos histogramas.

--- headers/histogramas.h
+++ headers/histogramas.h
@@ -1,10 +1,13 @@
+//Uso de ifndef, define y endif para evitar la duplicación de la librería/header
 #ifndef HISTOGRAMAS_H
 #define HISTOGRAMAS_H
 
-extern float promedioEstudiantes[40]; 
+extern float promedioEstudiantes[40]; //Variable extern: Para su uso en cualquier archivo. Variable que se encuentra definida en otro lugar pero con acceso a ella. 
 
 // void calcularPromedios(int Calificaciones[40][5], int Estudiantes, int Evaluaciones);
-void asignarPosicion(float promedio);
-void mostrarHistograma(int estudiantes);
-void iniciarHistograma(int Estudiantes, int Evaluaciones);
+
+//Declaración de funciones necesarias para definirlas en histogramas.c.
+void asignarPosicion(float promedio); // Asigna la cantidad de promedios dentro de un rango en la posición correspondiente.
+void mostrarHistograma(int estudiantes); // Empieza a dibujar el histograma.
+void iniciarHistograma(int Estudiantes, int Evaluaciones); // La función principal que dará inicio al proceso de generación del histograma.
 #endif
--- sources/histogramas.c
+++ sources/histogramas.c
@@ -1,9 +1,11 @@
+// Importación de librerias para la correcta función de la librería
 #include <stdio.h>
 #include <stddef.h>
 #include <Windows.h> 
 #include "../headers/histogramas.h"
 
-short int rangos[5] = {0};
+short int rangos[5] = {0}; // Variable que fungirá como indicador de rangos dependiendo la posición(0-59, 60-69, 70-79...)
+
 // static void calcularPromedios(int calificaciones[40][5], int estudiantes, int evaluaciones){
 //     int promedio = 0;
 //     for(size_t f = 0; f <= estudiantes; f++){
@@ -16,13 +18,13 @@ short int rangos[5] = {0};
 // }
 
 static void asignarPosicion(float promedio){
-    short int r1 = 0, r2 = 59;
-    for(size_t i = 0; i < 5; i++){
-        if(promedio >= r1 && promedio <= r2){
-            rangos[i]++;
-            break;
+    short int r1 = 0, r2 = 59; // Variables necesarias para ir asignando el rango.
+    for(size_t i = 0; i < 5; i++){// Ciclo for para ir recorriendo el vector rangos
+        if(promedio >= r1 && promedio <= r2){// Preguntamos si el valor que presente la variable promedio está dentro de un rango delimitado empezando de 0 a 59.
+            rangos[i]++;//Si un promedio se encuentra dentro del rango, la posición en la que se encuentre el vector (que a su vez representa un rango) acumulará +1 unidad (representando la cantidad de promedios en respectivo rango)
+            break;//Salimos del for para evitar recorrer todo el vector rangos si este ya encontró un rango.
         }
-        else{
+        else{//En caso de que el promedio no se encuentre dentro de un rango, se actualizarán los rangos a comparar en la proxima iteración.
             r1 = r2+1;
             r2 += 10;
         }
@@ -34,17 +36,16 @@ static void mostrarHistograma(int estudiantes){
     int porcentaje = 0;
     for (size_t i = 0; i < 5; i++)
     {
-        printf("%d ", rangos[i]);
-        porcentaje = ((rangos[i]*100)/estudiantes);
+        printf("%d ", rangos[i]); //Imprime la cantidad de promedios que hay en el rango correspondiente.
+        porcentaje = ((rangos[i]*100)/estudiantes); //Calcula el porcenje respecto a la cantidad de promedios existentes
         j = 0;
-        while (j <= porcentaje)
+        while (j <= porcentaje) //La cantidad de guiones que se generarán respecto al porcentaje.
         {
-            printf("-");
-            Sleep(25);
+            printf("-"); //Simula una barra.
+            Sleep(25); //"Dormimos" al sistema para crear una animación pequeña de generación del histograma.
         }
-        printf(" %.2f%%", porcentaje);
+        printf(" %.2f%%", porcentaje); // Imprimimos el porcentaje que le corresponde a un rango.
     }
-    
 }
 
 void iniciarHistograma(int Estudiantes, int Evaluaciones){
